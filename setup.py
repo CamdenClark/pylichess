96bceb2ace4dac1ea24b8b24406d830ef0770039
CamdenClark <camdenclark2012@gmail.com>
2015-12-17 00:17:16 -0800

    Changed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 		name="pylichess",
-		version="0.1",
+		version="0.2",
 		author="Camden Clark <camdenclark2012@gmail.com>",
 		install_requires=['simplejson'],
 		description=("Python wrapper for the lichess.com api."),
